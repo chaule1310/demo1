369b7d91e775c2d570a6c644559c8a14493511c4
Chaule1310 <peterchau1310@gmail.com>
2023-12-13 20:56:43 +0700

    commit baitapday1

---
+++ src/main/java/annotationbasedconfiguration/AccountService2Impl.java
@@ -0,0 +1,37 @@
+package annotationbasedconfiguration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pojobeans.Account;
+import pojobeans.AccountRepsitory;
+import pojobeans.AccountService;
+
+@Service("accountService")
+public class AccountService2Impl implements AccountService {
+    @Autowired
+    private AccountRepsitory accountRepsitory;
+    public void setAccountRepsitory(AccountRepsitory accountRepsitory) {
+        this.accountRepsitory = accountRepsitory;
+    }
+    @Override
+    public void transferMoney(long fromAccountId, long toAccountId, double amount) {
+        Account sourceAccount = accountRepsitory.find(fromAccountId);
+        Account targetAccount = accountRepsitory.find(toAccountId);
+        sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+        targetAccount.setBalance(targetAccount.getBalance() + amount);
+        accountRepsitory.update(sourceAccount);
+        accountRepsitory.update(targetAccount);
+    }
+
+    @Override
+    public void depositMoney(long accountId, double amount) throws Exception {
+        Account account =accountRepsitory.find(accountId);
+        account.setBalance(account.getBalance()+ amount);
+        accountRepsitory.update(account);
+    }
+
+    @Override
+    public Account getAccount(long accountId) {
+        return accountRepsitory.find(accountId);
+    }
+}
